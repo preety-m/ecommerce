e8f2c9a8b0758185cf679d5410b6e9aab1609291
preetydevim2004-maker <preetydevim2004@gmail.com>
2026-02-16 10:47:54 +0530

    setup views,admin,settings,urls

--- amazon/views.py
+++ amazon/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+def product_detail(request,id):
+    product=Product.object.get(id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
