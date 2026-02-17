71b8cc3dca376d5f6b15d9779bd579fa952a8967
preetydevim2004-maker <preetydevim2004@gmail.com>
2026-02-17 11:01:31 +0530

    further modified

--- amazon/models.py
+++ amazon/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Product(models.Model):
-    name=models.CharField(max_length=50)
-    price= models.DecimalField(max_digits=8,decimal_places=2)
-    description=models.TextField()
-    image=models.ImageField(upload_to='products/')
-    
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product.name
--- amazon/views.py
+++ amazon/views.py
@@ -1,5 +1,10 @@
+
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product
+from .models import Product ,Order, OrderItem
+from .forms import RegisterForm, CheckoutForm
 
 def product_list(request):
     products = Product.objects.all()
@@ -74,3 +79,49 @@ def remove(request, id):
 
     request.session['cart'] = cart
     return redirect('view_cart')
+def register(request):
+    if request.method == "POST":
+        form =RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=RegisterForm()
+    return render(request,'register.html',{'form':form})
+def user_login(request):
+    if request.method == "POST":
+        form=AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('product_list')
+    else:
+        form=AuthenticationForm()
+    return render(request,'login.html',{'form':form})
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+@login_required
+def checkout(request):
+    cart=request.session.get('cart',{})
+    if request.method =="POST":
+        form = CheckoutForm(request.POST)
+        if form.is_valid():
+            order=Order.objects.create(user=request.user,total_amount=0)
+            for id in cart:
+                product = Product.objects.get(id=id)
+                quantity=cart[id]
+                total+=product.price*quantity
+                OrderItem.objects.create(
+                    order=order,
+                    product=product,
+                    quantity=quantity
+                )
+                order.total_amount=total
+                order.save()
+                request.session['cart']={}
+                return redirect('product_list')
+    else:
+        form = CheckoutForm()
+    return render(request,'checkout.html',{'form':form})
+    
